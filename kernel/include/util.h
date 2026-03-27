a5ca163edc7988ed21df8de63985660d1a63d048
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-03-27 19:52:00 +0800

    kernel: prepare for legacy support
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

---
+++ kernel/include/util.h
@@ -0,0 +1,30 @@
+#ifndef __KSU_H_UTIL
+#define __KSU_H_UTIL
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 11, 0)
+#define ksu_close_fd close_fd
+#else
+#define ksu_close_fd ksys_close
+#endif
+
+static inline struct file *ksu_filp_open_nonotify(const char *path, int flags)
+{
+	struct path p;
+	struct file *f;
+	int ret;
+	ret = kern_path(path, (flags & O_NOFOLLOW) ? 0 : LOOKUP_FOLLOW, &p);
+	if (ret) {
+		return ERR_PTR(ret);
+	}
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
+	f = dentry_open_nonotify(&p, flags, current_cred());
+#else
+	f = dentry_open(&p, flags | __FMODE_NONOTIFY, current_cred());
+#endif
+
+	path_put(&p);
+	return f;
+}
+
+#endif // __KSU_H_UTIL
