bb4f545dd7004e4d7a1c70b1f0bbac89564fbd58
black-OWL432 <leecc0503@gmail.com>
2026-08-20 22:47:00 +0800

    manager: more customization
    * manager: bring back classic home ui
    * manager: add switch icon toggle
    * manager: add scroll/fade animation switcher
    
    Signed-off-by: KOWX712 <leecc0503@gmail.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/SettingsViewModel.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/viewmodel/SettingsViewModel.kt
@@ -46,6 +46,9 @@ class SettingsViewModel(
             val pageScale = repo.pageScale
             val enableWebDebugging = repo.enableWebDebugging
             val enableOfficialLauncher = repo.enableOfficialLauncher
+            val classicUi = repo.classicUi
+            val showSwitchIcon = repo.showSwitchIcon
+            val scrollAnimation = repo.scrollAnimation
             val colorStyle = repo.colorStyle
             val colorSpec = repo.colorSpec
             val isLkmMode = repo.isLkmMode()
@@ -86,6 +89,9 @@ class SettingsViewModel(
                     miuixMonet = miuixMonet,
                     keyColor = keyColor,
                     enableOfficialLauncher = enableOfficialLauncher,
+                    classicUi = classicUi,
+                    showSwitchIcon = showSwitchIcon,
+                    scrollAnimation = scrollAnimation,
                     enablePredictiveBack = enablePredictiveBack,
                     enableBlur = enableBlur,
                     enableFloatingBottomBar = enableFloatingBottomBar,
@@ -207,6 +213,21 @@ class SettingsViewModel(
     fun setEnableOfficialLauncher(enabled: Boolean) {
         repo.enableOfficialLauncher = enabled
         _uiState.update { it.copy(enableOfficialLauncher = enabled) }
+    }
+
+    fun setClassicUi(enabled: Boolean) {
+        repo.classicUi = enabled
+        _uiState.update { it.copy(classicUi = enabled) }
+    }
+
+    fun setShowSwitchIcon(enabled: Boolean) {
+        repo.showSwitchIcon = enabled
+        _uiState.update { it.copy(showSwitchIcon = enabled) }
+    }
+
+    fun setScrollAnimation(enabled: Boolean) {
+        repo.scrollAnimation = enabled
+        _uiState.update { it.copy(scrollAnimation = enabled) }
     }
 
     fun setEnablePredictiveBack(enabled: Boolean) {
