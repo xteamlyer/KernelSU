bd7c7f0f736255df2602f6bcb34df3488c1e4b28
KOWX712 <leecc0503@gmail.com>
2026-08-29 23:35:52 +0800

    ksud: add avc spoof to feature

--- userspace/ksud/src/feature.rs
+++ userspace/ksud/src/feature.rs
@@ -22,6 +22,7 @@ pub enum FeatureId {
     AdbRoot = 3,
     SelinuxHide = 4,
     WebviewZygoteUmount = 5,
+    AvcSpoof = 10003,
 }
 
 impl FeatureId {
@@ -33,6 +34,7 @@ impl FeatureId {
             3 => Some(Self::AdbRoot),
             4 => Some(Self::SelinuxHide),
             5 => Some(Self::WebviewZygoteUmount),
+            10003 => Some(Self::AvcSpoof),
             _ => None,
         }
     }
@@ -45,6 +47,7 @@ impl FeatureId {
             Self::AdbRoot => "adb_root",
             Self::SelinuxHide => "selinux_hide",
             Self::WebviewZygoteUmount => "webview_zygote_umount",
+            Self::AvcSpoof => "avc_spoof",
         }
     }
 
@@ -66,6 +69,9 @@ impl FeatureId {
             Self::WebviewZygoteUmount => {
                 "WebView Zygote Umount - unmount modules from WebView zygote and its isolated children"
             }
+            Self::AvcSpoof => {
+                "AVC Spoof - fix selinux context leak due to avc denial"
+            }
         }
     }
 }
@@ -78,6 +84,7 @@ fn parse_feature_id(name: &str) -> Result<FeatureId> {
         "adb_root" | "3" => Ok(FeatureId::AdbRoot),
         "selinux_hide" | "4" => Ok(FeatureId::SelinuxHide),
         "webview_zygote_umount" | "5" => Ok(FeatureId::WebviewZygoteUmount),
+        "avc_spoof" | "10003" => Ok(FeatureId::AvcSpoof),
         _ => bail!("Unknown feature: {name}"),
     }
 }
@@ -325,6 +332,7 @@ pub fn list_features() {
         FeatureId::AdbRoot,
         FeatureId::SelinuxHide,
         FeatureId::WebviewZygoteUmount,
+        FeatureId::AvcSpoof,
     ];
 
     for feature_id in &all_features {
@@ -389,6 +397,7 @@ pub fn save_config() -> Result<()> {
         FeatureId::AdbRoot,
         FeatureId::SelinuxHide,
         FeatureId::WebviewZygoteUmount,
+        FeatureId::AvcSpoof,
     ];
 
     for feature_id in &all_features {
