9872b3e6e13afe964e249cc9369dbd648e3a4855
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-09-10 13:21:10 +0800

    kernel: supercalls/debug: expose ksu_set_manager_appid to sys_reboot
    this is for debug and testing, this is locked to uid0 only anyway so I dont see issues.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/supercall/supercall.c
+++ kernel/supercall/supercall.c
@@ -16,6 +16,7 @@
 #include "arch.h"
 #include "util.h"
 #include "klog.h" // IWYU pragma: keep
+#include "manager/manager_identity.h"
 
 #define KSU_DRIVER_PERMISSION_SU_SESSION (1UL << 0)
 
@@ -144,6 +145,25 @@ static int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void 
     }
 
     // downstream: extensions go here!
+
+    // extensions
+    u64 reply = (u64)*arg;
+
+    if (magic2 == CHANGE_MANAGER_UID) {
+        // only root is allowed for this command
+        if (current_uid().val != 0)
+            return 0;
+
+        pr_info("sys_reboot: ksu_set_manager_appid to: %d\n", cmd);
+        ksu_set_manager_appid(cmd);
+
+        if (cmd == ksu_get_manager_appid()) {
+            if (copy_to_user((void __user *)*arg, &reply, sizeof(reply)))
+            	pr_info("sys_reboot: reply fail\n");
+        }
+
+        return 0;
+    }
 
     return 0;
 }
